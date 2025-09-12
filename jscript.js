dc4596ca30ba489569027107b6fc87466bb8d6a0
Sandra Köberl <sandra.koeberl@iu-study.org>
2025-09-12 18:10:52 +0200

    Version vom 12.09.2025

--- jscript.js
+++ jscript.js
@@ -83,17 +83,33 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-// Menü Toggle für mobile Geräte
-const toggleBtn = document.getElementById('menuToggle');
-const menuItems = document.getElementById('menuItems');
+// Funktion: On Click Button 2
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("FormularAbsenden");
+  const ausgabe = document.getElementById("anfrage-abgesendet");
 
-toggleBtn.addEventListener('click', () => {
-  menuItems.classList.toggle('d-none');
+  button.addEventListener("click", function () {
+    const suchbegriff = document.getElementById("anfrage").value;
+
+    if (suchbegriff.length < 50) {
+      alert("Ihre Anfrage muss aus mindestens 50 Zeichen bestehen!");
+    } else {
+      ausgabe.innerText = "OK";
+    }
+  });
 });
 
+// Menü Toggle für mobile Geräte
+//const toggleBtn = document.getElementById('menuToggle');
+//const menuItems = document.getElementById('menuItems');
+
+//toggleBtn.addEventListener('click', () => {
+//  menuItems.classList.toggle('d-none');
+//});
+
 // Schreibrichtung prüfen und Menü rechts positionieren, falls rtl
-if (document.documentElement.dir === 'rtl') {
-  document.getElementById('menu').classList.remove('start-0');
-  document.getElementById('menu').classList.add('end-0');
-}
+//if (document.documentElement.dir === 'rtl') {
+  //document.getElementById('menu').classList.remove('start-0');
+  //document.getElementById('menu').classList.add('end-0');
+//}
 
